b97f2869a99485e855819ea4bf8246aced745af0
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2024-09-16 17:52:37 +0300

    fix basename

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/bite-todo-react">
+        <BrowserRouter basename="/tasks">
           <Global styles={GlobalStyles} />
           <App />
         </BrowserRouter>
